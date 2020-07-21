d6708622ec2ee9a5a3cb6514d65b664aa12ceea2
zethporter <zethieboy@gmail.com>
2020-07-20 18:20:56 -0600

    Removed config file, didn't work as expected

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import App from './start.js';
 
 ReactDOM.render(<App />, document.getElementById('app'));
